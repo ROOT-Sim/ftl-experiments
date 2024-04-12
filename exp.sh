c11770c848729ed901583964a42a256dca365880
Romolo Marotta <romolo.marotta@gmail.com>
2024-04-12 12:23:32 +0200

    added kick the tires command
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- exp.sh
+++ exp.sh
@@ -13,7 +13,7 @@ fi
 
 if [ "$#" != 1 ]; then
     echo "Passed $# parameter(s) instead of 1"
-    echo "usage: ./exp <build|run_all|run_balanced|run_unbalanced|run_alternating|run_ftl|run_cpu|run_gpu|plot_all>"
+    echo "usage: ./exp <build|run_all|run_balanced|run_unbalanced|run_alternating|run_ftl|run_cpu|run_gpu|plot_all|report>"
     exit
 fi
 
@@ -40,24 +40,35 @@ if [[ "$1" == "build" ]]; then
   echo "#define NUM_LPS ($LPS*1024)" >> configs/balanced.h
   cp configs/balanced.h configs/unbalanced.h
   cp configs/balanced.h configs/alternating.h
+  cp configs/balanced.h configs/kick.h
 
   echo "#define ENABLE_HOT 1"        >> configs/balanced.h
   echo "#define PHASE_WINDOW_SIZE (1)" >> configs/balanced.h
   echo "#define HOT_PHASE_PERIOD (80*1000*1000)"  >> configs/balanced.h
+  echo "#define END_SIM_GVT  (64*1000*1000)" >> configs/balanced.h
 
   echo "#define ENABLE_HOT 1"        >> configs/unbalanced.h
   echo "#define PHASE_WINDOW_SIZE (80*1000*1000)" >> configs/unbalanced.h
   echo "#define HOT_PHASE_PERIOD 2"  >> configs/unbalanced.h
+  echo "#define END_SIM_GVT  (64*1000*1000)" >> configs/unbalanced.h
 
   echo "#define ENABLE_HOT 1"        >> configs/alternating.h
   echo "#define PHASE_WINDOW_SIZE (8*1000*1000)" >> configs/alternating.h
   echo "#define HOT_PHASE_PERIOD 2"  >> configs/alternating.h
+  echo "#define END_SIM_GVT  (64*1000*1000)" >> configs/alternating.h
+
+
+  echo "#define ENABLE_HOT 1"        >> configs/kick.h
+  echo "#define PHASE_WINDOW_SIZE (8*1000*100)" >> configs/kick.h
+  echo "#define HOT_PHASE_PERIOD 2"  >> configs/kick.h
+  echo "#define END_SIM_GVT  (32*1000*100)" >> configs/kick.h
 
 
   echo ###### building binaries #########
   for j in $WORKLOADS; do
     cp configs/$j.h rootsim_gputw/src/cuda/phold/test_config.h && make -j8 -C sim_build && cp sim_build/test/test_phold bins/$j
   done
+  cp configs/kick.h rootsim_gputw/src/cuda/phold/test_config.h && make -j8 -C sim_build && cp sim_build/test/test_phold bins/kick
 fi
 
 for j in $WORKLOADS; do
@@ -112,3 +123,17 @@ if [[ "$1" == "plot_$run" || "$1" == "plot_all" ]]; then
   done
 fi
 done
+
+
+if [[ "$1" == "kick" ]]; then
+    echo python3 rootsim_gputw/measure_energy.py ./bins/kick 3  | tee run_logs/kick.txt
+    python3 rootsim_gputw/measure_energy.py ./bins/kick 3 | tee run_logs/kick.txt
+fi
+
+
+if [[ "$1" == "report" ]]; then
+echo "\item CPU:" $(lscpu | grep "Model name:" | cut -d':' -f2)                 > run_logs/machine.tex
+echo "\item GPU:" $(lspci | grep "VGA compatible controller:" | cut -d':' -f3) >> run_logs/machine.tex
+echo "\item RAM:" $(lsmem | grep "Total online memory:" | cut -d':' -f2)       >> run_logs/machine.tex
+pdflatex report.tex
+fi
