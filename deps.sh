f589f12cba62441789c14b7bd3edd40817c02cd6
Romolo Marotta <romolo.marotta@gmail.com>
2024-05-27 16:15:51 +0200

    rcr(pads2024): improved deps installing
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 
-sudo apt-get -y install git cuda-toolkit-12-5 nvidia-driver-555-open cuda-drivers-555 cmake \
+sudo apt-get -y install git gcc g++ cuda-toolkit-12-5 nvidia-driver-555-open cuda-drivers-555 cmake \
                         python3-full python3-pip python3-venv gnuplot fonts-linuxlibertine \
                         texlive-latex-base  texlive-science texlive-latex-recommended texlive-formats-extra
